9024462a2f60126c3fb739bf2440d001ec67752a
Di Cui <cuidi0315@gmail.com>
2026-07-30 15:04:17 -0400

    Delete Basic Joins directory

--- Basic Joins/1378_replace_employee_id.sql
+++
@@ -1,6 +0,0 @@
---- 1378. Replace Employee ID With The Unique Identifier
---- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/
-SELECT EmployeeUNI.unique_id, Employees.name
-FROM Employees
-LEFT JOIN EmployeeUNI
-ON EmployeeUNI.id = Employees.id

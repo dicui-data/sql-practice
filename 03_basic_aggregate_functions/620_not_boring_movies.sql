511948327492fef5c63fc695d75eaa309928748d
Di Cui <cuidi0315@gmail.com>
2026-07-30 15:07:45 -0400

    Create 620_not_boring_movies.sql

---
+++ 03_basic_aggregate_functions/620_not_boring_movies.sql
@@ -0,0 +1,12 @@
+-- 620. Not Boring Movies
+-- https://leetcode.com/problems/not-boring-movies/
+SELECT	
+	*
+FROM	
+	Cinema
+WHERE	
+	id % 2 = 1
+AND	
+	description != 'boring'
+ORDER BY	
+	rating DESC

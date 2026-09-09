ed2b9758162a3659b959a6d1a5cc7fc4ac915f56
Di Cui <cuidi0315@gmail.com>
2026-09-09 00:05:23 -0400

    Create 1633_percentage_of_users_attended_a_contest.sql

---
+++ 03_basic_aggregate_functions/1633_percentage_of_users_attended_a_contest.sql
@@ -0,0 +1,20 @@
+-- 1633. Percentage of Users Attended a Contest
+-- https://leetcode.com/problems/percentage-of-users-attended-a-contest/
+SELECT 
+  contest_id, -- The ID of the contest
+  ROUND(
+    COUNT(DISTINCT user_id) * 100 / ( -- Calculate the percentage of users
+      SELECT 
+        COUNT(user_id) -- Total number of unique users
+      FROM 
+        Users
+    ), 
+    2
+  ) AS percentage -- The percentage of users registered for each contest, rounded to 2 decimal places
+FROM 
+  Register -- The table containing registration information
+GROUP BY 
+  contest_id -- Group the data by contest ID
+ORDER BY 
+  percentage DESC, -- Order the results by percentage in descending order
+  contest_id; -- Then order by contest ID for ties
